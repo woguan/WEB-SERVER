d54c9fb436c9cea681038a35915b414ecdd68b60
woguan <woguan@users.noreply.github.com>
2016-08-15 01:27:06 -0700

    Update get.php

--- get.php
+++ get.php
@@ -64,7 +64,7 @@ else if ($first == '' || $last == ''){
 
 echo "Hello $last $first. Nice to meet you. Your Favorite color is: $color";
 //echo "<script>setBackGroundColor('$color')</script>";
-echo "<script>document.body.style.backgroundColor="Blue"</script>";
+echo "<script>document.body.style.backgroundColor=\"Blue\"</script>";
 ?>
 
  <br>
