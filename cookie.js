10e12b0e2fa5c736bd4e4037a89a5228980cc90d
woguan <woguan@users.noreply.github.com>
2016-08-14 22:29:34 -0700

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -2,7 +2,7 @@
 
 function delcookie(){
   //document.body.style.backgroundColor = "#fffe1b";
-  document.body.style.backgroundColor = brown;
+  document.body.style.backgroundColor = "Brown";
   document.cookie = "Cookie_First_Name=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
   document.cookie = "Cookie_Last_Name=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
   document.cookie = "Cookie_Favorite_Color=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
