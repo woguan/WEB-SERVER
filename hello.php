5c05e8f97ee584e0b318bc257bcec55722ebb024
woguan <woguan@users.noreply.github.com>
2016-08-14 23:59:43 -0700

    Update hello.php

--- hello.php
+++ hello.php
@@ -12,6 +12,13 @@
 <meta name="viewport" content="width = device-width, initial-scale = 1">
 <title>PHP</title>
  
+ <style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+
+
 </head>
 
 <body>
@@ -24,11 +31,18 @@ echo "The current server timezone is: " . $date;
  echo "<br />\n";
 
 
-    foreach ($_SERVER as $key=>$val )
+    
+  
+echo  "<table>
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+  </tr>";
+ foreach ($_SERVER as $key=>$val )
        {
          echo "<tr><td>".$key."</td><td>" .$val."</tr>";
         }
-  
+echo  "</table>"
 
        ?>
        
