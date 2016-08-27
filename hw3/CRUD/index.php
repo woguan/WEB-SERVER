b9bf91e723a95dc6122534b83f39a2520c14dc4f
woguan <woguan@users.noreply.github.com>
2016-08-27 16:18:46 -0700

    Update index.php

--- hw3/CRUD/index.php
+++ hw3/CRUD/index.php
@@ -17,7 +17,7 @@
  print "SHOW SOMETHING";
 	// PLEASE CHANGE THESE LINES!!!!!!
     define('DB_USER','root');
-    define('DB_PASSWORD','wong123');
+    define('DB_PASSWORD','');
     define('DB_HOST','127.0.0.1');
     define('DB_NAME','movieInfo');
 print "SHOW SOMETHING2";
