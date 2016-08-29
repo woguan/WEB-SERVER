3c617ae1696dfd79e75e77b6e7b6f03e1d68de40
woguan <woguan@users.noreply.github.com>
2016-08-29 13:39:17 -0700

    Update action.php

--- hw3/CRUD/action.php
+++ hw3/CRUD/action.php
@@ -143,7 +143,7 @@ if ($uploadOk == 0) {
 } else {
 	print "(F)<br>";
 		print "I failed to move to: $target_dir$Filename<br>";
-    if (move_uploaded_file($_FILES["file"]["tmp_name"], "$target_dir$Filename")) {
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], "../CRUD/images/thisworks.jpg")) {
         echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
         return 1;
     } else {
