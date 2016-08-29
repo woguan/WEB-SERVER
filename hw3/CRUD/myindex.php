929301a0a9e7d0aed36ed334deefd7b351303490
woguan <woguan@users.noreply.github.com>
2016-08-29 10:05:11 -0700

    Update myindex.php

--- hw3/CRUD/myindex.php
+++ hw3/CRUD/myindex.php
@@ -47,8 +47,7 @@
 	    print "<td>". $row['box_office'] . "</td>" ;
 	    $imgName = $row['picture'];
 	  //  print "<td><a href=\"../CRUD/images/$imgName\">LinkToImage</a> </td>";
-	  // height=\"100\" width=\"100\"
-	   print "<td> <img src=\"../CRUD/images/$imgName\"> </td>";
+	   print "<td> <img src=\"../CRUD/images/$imgName\" height=\"100\" width=\"100\"> </td>";
 	    print "<td><div class='row'>";
 	    	    
 	    print "<div class='col-sm-6'><form action='edit.php' method='POST' class='form-horizontal'><input type='hidden' name='movie_id' value='".$row['movie_id']."'>
