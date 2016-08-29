935892a4f117f8a4932732c802763b42a9b262ad
woguan <woguan@users.noreply.github.com>
2016-08-29 13:07:21 -0700

    Update action.php

--- hw3/CRUD/action.php
+++ hw3/CRUD/action.php
@@ -12,68 +12,6 @@
     }
 
 	$action = $_REQUEST['action'];
-    
-    // Helper Functions
-    function uploadIMG(string $target_dir, string $Filename, string $imageFileType) {
-   print "This are the info: <br>";
-   print "$target_dir <br>";
-   print "$Filename <br>";
-   print "$imageFileType <br>";
-  print basename($_FILES["file"]["name"]);
-  print "<br>";
-  //print "$imageFileType <br>";
-  //print "$imageFileType <br>";
-  
-  
-          $uploadOk = 1; // this is a boolean to tell if upload is valid
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-	 print "(A)<br>";
-    $check = getimagesize($_FILES["file"]["tmp_name"]);
-    if($check !== false) {
-    	print "(B)<br>";
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-    	print "(C)<br>";
-        $msgerr = "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-
-$chsz = getimagesize($_FILES["file"]["size"]);
-print $chsz;
-
-// Check file size
-if ($_FILES["file"]["size"] > 5000000) {
-    $msgerr = "Sorry, your file is over 5MB.";
-    print "(100)<br>";
-    $uploadOk = 0;
-}
-// Only allow 3 extensions - jpg, png, jpeg
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-    print "(101)<br>";
-    $msgerr = "Sorry, only JPG, JPEG, PNG files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-	print "(D)<br>";
-} else {
-	print "(F)<br>";
-		print "I failed to move to: $target_dir$Filename<br>";
-    if (move_uploaded_file($_FILES["file"]["name"], "$target_dir$Filename")) {
-        echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
-        return 1;
-    } else {
-    	print "(E)<br>";
-        $msgerr = "Sorry, there was an error uploading your file.";
-    }
-	
-}
- return 0;    
-     
-}
       
 // my globals
 	$msgerr = ''; //Error Reporting Variable
@@ -154,4 +92,68 @@ if ($isActionExecuted == 1){
 else{
 	print "An error has occured. $msgerr";
 }
+
+
+    // Helper Functions
+    function uploadIMG(string $target_dir, string $Filename, string $imageFileType) {
+   print "This are the info: <br>";
+   print "$target_dir <br>";
+   print "$Filename <br>";
+   print "$imageFileType <br>";
+  print basename($_FILES["file"]["name"]);
+  print "<br>";
+  //print "$imageFileType <br>";
+  //print "$imageFileType <br>";
+  
+  
+          $uploadOk = 1; // this is a boolean to tell if upload is valid
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+	 print "(A)<br>";
+    $check = getimagesize($_FILES["file"]["tmp_name"]);
+    if($check !== false) {
+    	print "(B)<br>";
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+    	print "(C)<br>";
+        $msgerr = "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
+$chsz = getimagesize($_FILES["file"]["size"]);
+print $chsz;
+
+// Check file size
+if ($_FILES["file"]["size"] > 5000000) {
+    $msgerr = "Sorry, your file is over 5MB.";
+    print "(100)<br>";
+    $uploadOk = 0;
+}
+// Only allow 3 extensions - jpg, png, jpeg
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    print "(101)<br>";
+    $msgerr = "Sorry, only JPG, JPEG, PNG files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+	print "(D)<br>";
+} else {
+	print "(F)<br>";
+		print "I failed to move to: $target_dir$Filename<br>";
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], "$target_dir$Filename")) {
+        echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
+        return 1;
+    } else {
+    	print "(E)<br>";
+        $msgerr = "Sorry, there was an error uploading your file.";
+    }
+	
+}
+ return 0;    
+     
+}
+
 ?>
