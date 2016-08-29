c5d3ca5769fcf92d934442440f196628e3aa4a7d
woguan <woguan@users.noreply.github.com>
2016-08-29 11:00:18 -0700

    Update myindex.php

--- hw3/CRUD/myindex.php
+++ hw3/CRUD/myindex.php
@@ -29,10 +29,10 @@
       
     }
     
-    print "show some data[2]: ";
+    print "show some data[3]: ";
      $sql2 = "SELECT count(movie_id) FROM movieInfo ";
          $retval3 = mysqli_query( $sql2, $conn );
-         $row5 = mysqli_fetch_array($retval3, MYSQLI_NUM );
+         $row5 = mysqli_fetch_array($retval3);
          $rec_count = $row5[0];
          print $rec_count;
          
