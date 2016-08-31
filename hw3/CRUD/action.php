3321b79f35f52c72e85fc4d3d1db32e500b0f5ca
woguan <woguan@users.noreply.github.com>
2016-08-31 09:06:22 -0700

    Update action.php

--- hw3/CRUD/action.php
+++ hw3/CRUD/action.php
@@ -115,7 +115,7 @@ print "the statement was executed;<br>";
 else{
 	$isActionExecuted = 0;
 	print "One of the inputs had problem<br>";
-	print mysqli_errno($mysqli);
+	print mysqli_error($mysqli);
 }
 $stmt->close();
 
