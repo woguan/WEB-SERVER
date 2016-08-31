03ce11212cf78b8538f78951a8ba886cd75b1ade
wong <woguan@ucsd.edu>
2016-08-31 20:08:37 +0000

    Updated and fixed edge cases. Need to finish CSS

--- hw3/CRUD/index2.php
+++ hw3/CRUD/index2.php
@@ -2,15 +2,15 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Super Basic CRUD!</title>
+<title>Box Office Tracker</title>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
 
 <div class="container">
+<h1 style="color:red; border:solid 2px blue; font-style:italic; text-align:center">Box Office Tracker</h1>
 	
-<h1>Super Basic CRUD!</h1>
 
 <?php
 
@@ -32,34 +32,32 @@
         $sql = "SELECT * FROM movieInfo";
         $retval = mysqli_query($conn, $sql);
         $rec_count = mysqli_num_rows($retval);
-        print "There are  $rec_count rows<br>";
+        //print "There are  $rec_count rows<br>";
 
 	// Code to set rec_limit
 	if( isset($_GET['rec_limit'])){
-	$rec_limit = $_GET['rec_limit'];}
+	$rec_limit = $_GET['rec_limit'];
+		if($rec_limit == 'All'){
+		$rec_limit = $rec_count;
+		}
+	}
 	else{
-        $rec_limit =5;}
+        $rec_limit =5;
+	}
 
         // Page number
         if( isset($_GET['page'] ) ) {
             $page = $_GET['page'] + 1 ;
             $offset = $rec_limit * $page ;
-
-        // Hard coded. Find more efficient way of doing this
-       /* if($page == 1){$rec_limit = 5;}
-        elseif($page == 2){$rec_limit = $rec_limit * 2;}
-        elseif($page == 3){$rec_limit = $rec_limit * 4;}
-        else{ $rec_limit = $rec_count;}
-        $offset = 0;*/
          }
 	else {
         $page = 0;
-        //$page = 1;
         $offset = 0;
-        //$rec_limit = 5;
-         }
-        print "rec_limit currently is $rec_limit<br>";
-        print "Page $page<br>";
+        }
+
+       // print "rec_limit currently is $rec_limit<br>";
+       // print "Page $page<br>";
+	
 
         // Checks for sort
         if( isset($_GET['sort'])){
@@ -91,42 +89,39 @@
          }
 
 	// Code for drop down menu
-	print "Show ";
-	print "<select id=\"dropdown\" onchange=\"reload()\"><option value=\"\"></option><option value=\"5\">5</option><option value=\"10\">10</option><option value=\"20\">20</option><option value=\"all\">All</option></select>";
-	print " entries";
+	print "Show "; 
+	?>
+	
+	<select id="dropdown" onchange="reload()">
+	<option value="5" <?PHP if ($rec_limit==5) echo ' selected="selected"';?>>5</option>
+	<option value="10" <?PHP if ($rec_limit==10) echo ' selected="selected"';?>>10</option>
+	<option value="20" <?PHP if ($rec_limit==20) echo ' selected="selected"';?>>20</option>
+	<option value="All" <?PHP if ($rec_limit==$rec_count) echo ' selected="selected"';?>>All</option>
+	</select>
 
-	// JS code which reads what is currently being selected from drop down menu
-/*	print "<script type=\"text/javascript\">
-		function reload(){
-		var tmp = document.getElementById(\"dropdown\").value;
-		window.location.href=\"$_PHP_SELF?page=2\";}
-		</script>";*/
+<?PHP
+	print " entries";
 
     // USE THE QUERY RESULT
     print "<table class='table'>";
     print "<tr><th><a href=\"$_PHP_SELF?page=$page&sort=0\">Movie Title</a></th><th><a href=\"$_PHP_SELF?page=$page&sort=1\">Studio</a></th><th><a href=\"$_PHP_SELF?page=$page&sort=2\">Year</a></th>";
     print "<th><a href=\"$_PHP_SELF?page=$page&sort=3\">Box Office $</a></th><th><a href=\"$_PHP_SELF?page=$page&sort=4\">Picture</a></th></tr>";
 
-   // if (mysqli_num_rows($result) > 0) {
-if($rec_count > 0){
-
-
+    if($rec_count > 0){
       while($row = mysqli_fetch_assoc($result)) {
-//      while($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
             print "<tr>";
             print "<td style=\"vertical-align:middle;\">". $row['movie_title'] . "</td>" ;
             print "<td style=\"vertical-align:middle;\">". $row['studio'] . "</td>" ;
             print "<td style=\"vertical-align:middle;\">". $row['year'] . "</td>" ;
             print "<td style=\"vertical-align:middle;\">". $row['box_office'] . "</td>" ;
             $imgName = $row['picture'];
-            //print "<td><a href=\"../CRUD/images/$imgName\">LinkToImage</a> </td>";
             
-             if ($imgName !== ''){
-	   print "<td> <img id=\"noimagefound\" src=\"../CRUD/images/$imgName\" height=\"150\" width=\"100\" alt=\"No Image\" onerror=\"showNoImage()\"> </td>";
-	  }else{
+            if ($imgName !== ''){
+	    print "<td> <img id=\"noimagefound\" src=\"../CRUD/images/$imgName\" height=\"150\" width=\"100\" alt=\"No Image\" onerror=\"showNoImage()\"> </td>";
+	  }
+	    else{
 	  print "<td> <img id=\"noimagefound\" src=\"../CRUD/images/noimage.png\" height=\"150\" width=\"100\" alt=\"No Image\" onerror=\"showNoImage()\"> </td>";	
 	  }
-            
             
             print "<td style=\"vertical-align:middle;\"><div class='row'>";
 
@@ -144,20 +139,66 @@ if($rec_count > 0){
             print "<tr><td colspan='4'>No Rows</td></tr>";
     }
 
+
 	print "</table>";
 	$start_Val = $offset + 1;
 	$end_val = $rec_limit * ($page + 1);
 	$prev_page = $page - 2;
 	$last_page = ceil($rec_count/$rec_limit) - 2;
-	print "Showing $start_Val to $end_val of $rec_count entries"; // Fix rec_limit = 999 bug
-	print "<a href=\"$_PHP_SELF?rec_limit=$rec_limit\">First Page</a>";
-	print "<a href=\"$_PHP_SELF?page=$prev_page&rec_limit=$rec_limit\">Previous Page</a>"; // Fix bug where u can hit privious and not get data. Add if statement
-	print "<a href=\"$_PHP_SELF?page=$page&rec_limit=$rec_limit\">Next Page</a>";
-	print "<a href=\"$_PHP_SELF?page=$last_page&rec_limit=$rec_limit\">Last Page</a>";
 
-	// Still need to account for edge cases especially for the "All" selected option
+	// Prints next page
+	if($end_val > $rec_count){
+	print "<b>Showing $start_Val to $rec_count of $rec_count entries</b>";
+	}
+
+	else{
+	print "<b>Showing $start_Val to $end_val of $rec_count entries</b>";
+	}
 ?>
 
+<div style="border:dashed 2px black; font-size:1.5em; text-align:center">
+
+<?PHP
+	// Prints first page
+	if($page == 0){
+	print "<a href=\"$_PHP_SELF?rec_limit=$rec_limit\" onclick=\"return false\">First Page</a>";
+	}
+
+	else{
+	print "<a href=\"$_PHP_SELF?rec_limit=$rec_limit\">First Page</a>";
+	}
+
+	// Prints previous page
+	if($page < 1){
+	print "<a href=\"$_PHP_SELF?page=$prev_page&rec_limit=$rec_limit\" onclick=\"return false\">Previous Page</a>";
+	}
+
+	else{
+	print "<a href=\"$_PHP_SELF?page=$prev_page&rec_limit=$rec_limit\">Previous Page</a>";
+	}
+
+	// Prints next page
+	if($page > $last_page){
+	print "<a href=\"$_PHP_SELF?page=$page&rec_limit=$rec_limit\" onclick=\"return false\">Next Page</a>";
+	}
+
+	else{
+	print "<a href=\"$_PHP_SELF?page=$page&rec_limit=$rec_limit\">Next Page</a>";
+	}
+
+	// Prints last page
+	if($page > $last_page){
+//	print "<a href=\"$_PHP_SELF?page=$last_page&rec_limit=$rec_limit\" onclick=\"return false\" style=\"cursor:not-allowed\">Last Page</a>";
+	print "Last Page";
+	}
+
+	else{
+	print "<a href=\"$_PHP_SELF?page=$last_page&rec_limit=$rec_limit\">Last Page</a>";
+	}
+
+?>
+</div>
+<br><br>
 <form action="edit.php" method="POST">
 	<input type="submit" name="action" value="Add" class="btn btn-lg btn-primary">
 </form>	
@@ -166,30 +207,15 @@ if($rec_count > 0){
 <hr>
 <br><br>
 
-<div class="alert alert-danger" role="alert">
-	<h2>Things Missing</h2>
-	
-	<ol>
-		<li>Validation and more validation</li>
-		<li>Paging</li>
-		<li>Sorting</li>
-		<li>JavaScript (Required or Optional?)</li>
-		<li>As good as reasonable separation of code and template</li>
-		<li>Little code improvements</li>
-	</ol>
-</div>
-
-</div>
-
 </body>
 <script type="text/javascript">
                 function reload(){
                 var tmp = document.getElementById("dropdown").value;
 
-		if(tmp == 10){window.location.href="index2.php?rec_limit=10";}
+		if(tmp == 5){window.location.href="index2.php?rec_limit=5";}
+		else if(tmp == 10){window.location.href="index2.php?rec_limit=10";}
 		else if(tmp == 20){window.location.href="index2.php?rec_limit=20";}
-		else if(tmp == "all"){window.location.href="index2.php?rec_limit=9999";}
-		else if(tmp == 5){window.location.href="index2.php?rec_limit=5";}
+		else if(tmp == 'All'){window.location.href="index2.php?rec_limit=All";}
 		}
 </script>
 
